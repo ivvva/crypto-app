a9d9a96b3d79ffd0e6e3f91cebf901be5e1069c8
iva <ivvvpol@gmail.com>
2022-08-08 12:29:54 +0200

    Redux toolkit

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,11 +1,11 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
   'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
   'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com'
 }
 
-const baseUrl = 'https://coinranking1.p.rapidapi.com/coins'
+const baseUrl = 'https://coinranking1.p.rapidapi.com'
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
 
@@ -14,10 +14,14 @@ export const cryptoApi = createApi({
   baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: () => createRequest('/exchanges')
+      query: () => createRequest('/coins')
     })
   })
 })
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi
 // const options = {
 //   method: 'GET',
 //   url: 'https://coinranking1.p.rapidapi.com/coins',
